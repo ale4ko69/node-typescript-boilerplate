20479dd51fc3048a6458378ba29e7e1c5d551a91
ale4ko69 <alex@kagansky.me>
2023-05-03 17:05:28 +0300

    Test Error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { analytics } from './modules/analytics.js'
-import {alex} from './modules/index.js';
-const alex = "alex"
+import alex from './modules/index.js';
+// const alex = "alex"
 const message = 'Hello NodeJS'
 
 console.log(`${message} from ${alex}`)
